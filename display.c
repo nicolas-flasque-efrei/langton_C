8b97426cfdff07d44914ada594f00bc552e8da99
nicolas-flasque-efrei <nicolas.flasque@efrei.net>
2025-03-05 18:21:34 +0100

    add initial CMake configuration and project files for langton - portable use of console display with conio for windows and ncurses for linux/mac

--- display.c
+++ display.c
@@ -3,11 +3,12 @@
 //
 
 #include <stdio.h>
-#include <windows.h>
+#include "map.h"
 #include "display.h"
 
 // check for OS
 #if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
 #define FONT_SIZE 5
 void init_display()
 {
@@ -33,18 +34,19 @@ void putpixel(int x, int y, int color)
 void close_display() {}
 
 #else
+
 void init_display() { initscr(); resize_term(100, 100); start_color(); init_pair(1, COLOR_WHITE, COLOR_BLACK); init_pair(2, COLOR_BLACK, COLOR_BLACK);}
 
 void putpixel(int x, int y, int color)
 {
-    int pairnum = 0;
+    int pairnum = 2;
     if (color == WH)
     {
         pairnum = 1;
     }
     move(y, x);
-    attron(COLOR_PAIR(pairnum))
-    printw("%c", 219);
+    attron(COLOR_PAIR(pairnum));
+    printw("X");
     attroff(COLOR_PAIR(pairnum));
     refresh();
 }
